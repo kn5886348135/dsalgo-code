57715e02dd3c4b177cb7f4ead2aee64997949916
kn5886348135 <kn5886348135@163.com>
2020-09-07 20:59:45 +0800

    选择排序算法

--- src/main/java/com/example/dsalgo/sort/Sort.java
+++ src/main/java/com/example/dsalgo/sort/Sort.java
@@ -7,7 +7,15 @@ package com.example.dsalgo.sort;
  */
 public class Sort {
     public static void main(String[] args) {
+        int[] arr = {3, 85, 4168, 435, 1746, 3, 841351, 638168, 54684, 643, 163, 85164, 684668435};
+        int len = arr.length;
+//        bubbleSort(arr);
+//        insertionSort(arr);
+        selectionSort(arr);
 
+        for (int i = 0; i < len; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
     /**
@@ -55,6 +63,26 @@ public class Sort {
                 }
             }
             a[j + 1] = value;
+        }
+    }
+
+    public static void selectionSort(int[] a) {
+        int len = a.length;
+        if (len <= 1) {
+            return;
+        }
+
+        for (int i = 0; i + 1 < len; i++) {
+            int index = i;
+            for (int j = i + 1; j < len; j++) {
+                if (a[j] < a[index]) {
+                    index = j;
+                }
+            }
+            int temp = 0;
+            temp = a[i];
+            a[i] = a[index];
+            a[index] = temp;
         }
     }
 
