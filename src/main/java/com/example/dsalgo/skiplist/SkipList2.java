50bc9b57129bef0831fd6cfb99c4ae17dc1c11c7
kn5886348135 <kn5886348135@163.com>
2020-10-05 20:05:53 +0800

    链表的插入和删除

--- src/main/java/com/example/dsalgo/skiplist/SkipList2.java
+++ src/main/java/com/example/dsalgo/skiplist/SkipList2.java
@@ -58,6 +58,81 @@ public class SkipList2 {
         }
     }
 
+    public void insert2(int value){
+        int level = head.forwards[0] == null ? 1 : randomLevel();
+        if (level > levelCount) {
+            level = ++levelCount;
+        }
+        Node newNode = new Node(level);
+        newNode.data = value;
+        Node p = head;
+
+        for (int i = levelCount - 1; i >= 0; i--) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            if (level > i) {
+                if (p.forwards[i] == null) {
+                    p.forwards[i] = newNode;
+                }else {
+                    Node next=p.forwards[i];
+                    p.forwards[i] = newNode;
+                    newNode.forwards[i] = next;
+                }
+            }
+        }
+    }
+
+    public void insert(int value, int level) {
+        if (level == 0) {
+            level = randomLevel();
+        }
+
+        Node newNode = new Node(level);
+        newNode.data = value;
+        newNode.maxLevel = level;
+
+        Node update[] = new Node[level];
+        for (int i = 0; i < level; i++) {
+            update[i] = head;
+        }
+
+        Node p = head;
+        for (int i = level - 1; i >= 0; i--) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        for (int i = 0; i < level; i++) {
+            newNode.forwards[i]=update[i].forwards[i];
+            update[i].forwards[i] = newNode;
+        }
+
+        if (levelCount < level) {
+            levelCount = level;
+        }
+    }
+
+    public void delete(int value) {
+        Node[] update = new Node[levelCount];
+        Node p = head;
+        for (int i = levelCount - 1; i >= 0; i--) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value) {
+            for (int i = levelCount - 1; i >= 0; i--) {
+                if (update[i].forwards[i] != null && update[i].forwards[i].data == value) {
+                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
+                }
+            }
+        }
+    }
 
     private int randomLevel() {
         int level = 1;
@@ -67,6 +142,29 @@ public class SkipList2 {
             }
         }
         return level;
+    }
+
+    public void printAll(){
+        Node p=head;
+        while (p.forwards[0] != null) {
+            System.out.print(p.forwards[0]+" ");
+            p = p.forwards[0];
+        }
+        System.out.println();
+    }
+
+    public void printAll_beautiful(){
+        Node p=head;
+        Node[] c = p.forwards;
+        Node[] d = c;
+        int maxLevel = c.length;
+        for (int i = maxLevel - 1; i >= 0; i--) {
+            do {
+                System.out.print((d[i] != null ? d[i].data : null) + ":" + i + "-----");
+            } while (d[i] != null && (d = d[i].forwards)[i] != null);
+            System.out.println();
+            d = c;
+        }
     }
 
     /**
@@ -95,5 +193,26 @@ public class SkipList2 {
 
     public static void main(String[] args) {
         SkipList2 list = new SkipList2();
+        list.insert(1, 3);
+        list.insert(2, 3);
+        list.insert(3, 2);
+        list.insert(4, 4);
+        list.insert(5, 10);
+        list.insert(6, 4);
+        list.insert(8, 5);
+        list.insert(7, 4);
+        list.printAll_beautiful();
+        list.printAll();
+        SkipList2 list2 = new SkipList2();
+        list2.insert2(1);
+        list2.insert2(2);
+        list2.insert2(6);
+        list2.insert2(7);
+        list2.insert2(8);
+        list2.insert2(3);
+        list2.insert2(4);
+        list2.insert2(5);
+        System.out.println();
+        list2.printAll_beautiful();
     }
 }
